c21475648e8572c196f03c510e4e34ce0e2a5ead
Mohaned Gomaa <mohamed.gomaa@intalio.com>
2024-09-29 11:01:08 +0300

    -Add database connection into appsetting.json. -Configure database connection in program.cs. -Init database with identity schema. -Seed roles data inside database using migration.

---
+++ APIsJWTAuthentication/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIsJWTAuthentication.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, MaxLength(100)]
+        public required string FirstName { get; set; }
+
+        [Required, MaxLength(100)]
+        public required string LastName { get; set; }
+    }
+}
--- APIsJWTAuthentication/Program.cs
+++ APIsJWTAuthentication/Program.cs
@@ -1,9 +1,18 @@
 using APIsJWTAuthentication.Helpers;
+using APIsJWTAuthentication.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var confg = builder.Configuration;
 
-//builder.Services.Configure<JWT>(confg.GetSection("JWT"));
+builder.Services.Configure<JWT>(confg.GetSection("JWT"));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(confg.GetConnectionString("DefaultConnection"))
+);
 
 // Add services to the container.
 builder.Services.AddControllers();
