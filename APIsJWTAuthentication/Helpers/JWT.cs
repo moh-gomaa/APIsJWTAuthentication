e20239ada31bc30b2b1455f87690084134917c92
Mohaned Gomaa <mohamed.gomaa@intalio.com>
2024-09-29 10:17:54 +0300

    -Initialize .NET API web app. -Install required nuget packages for authentication. -Add JWT properties in appsetting.json.

---
+++ APIsJWTAuthentication/Helpers/JWT.cs
@@ -0,0 +1,10 @@
+﻿namespace APIsJWTAuthentication.Helpers
+{
+    public class JWT
+    {
+        public required string? Key { get; set; }
+        public required string Issuer { get; set; }
+        public required string Audience { get; set; }
+        public int DurationInDays { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# APIsJWTAuthentication
+ASP.NET Core 8 project which contain APIs with authentication using JWT (Json Web Token)
